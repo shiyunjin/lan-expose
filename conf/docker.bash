aed8cdca70d7d33b0a2c20f850258ff424951068
shiyunjin <admin@shiyunjin.cn>
2022-07-15 02:49:35 +0800

    revert(conf): docker conf udp port

--- conf/docker.bash
+++ conf/docker.bash
@@ -9,6 +9,7 @@ docker run -d --restart=always --name="lan-expose-proxy" \
   -v /volume1/docker/lan-expose-proxy/proxy.ini:/config/proxy.ini \
   -v /volume1/homes/syj/syno-acme/acme.sh/ **#YOUR DOMAIN#** /fullchain.cer:/config/ssl.crt \
   -v /volume1/homes/syj/syno-acme/acme.sh/ **#YOUR DOMAIN#** / **#YOUR DOMAIN#** .key:/config/ssl.key \
-  -p 690:690 \
+  -p 690:690/tcp \
+  -p 690:690/udp \
   ghcr.io/shiyunjin/lan-expose-proxy:test \
   -c /config/proxy.ini
